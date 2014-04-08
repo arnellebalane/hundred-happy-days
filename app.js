1ccb0c814db61f267ddb362c048bf2bc5dee0896
Arnelle Balane <arnellebalane@gmail.com>
2014-04-08 22:02:55 +0800

    added app config file

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+var config = require('./config')();
 var http = require('http');
 var path = require('path');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', config.port || process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -26,13 +19,9 @@ app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
